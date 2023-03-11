7296b58b350550eb78cfb13ec1bf116f0cd67365
jalalAzhmatkhan <jalaluddin.94@yahoo.com>
2023-03-11 21:55:28 +0700

    Fix start-reload.sh
    Add module name and variable name

--- backend/app/start-reload.sh
+++ backend/app/start-reload.sh
@@ -13,4 +13,14 @@ HOST=${HOST:-0.0.0.0}
 PORT=${PORT:-5080}
 LOG_LEVEL=${LOG_LEVEL:-info}
 
-exec nohup uvicorn --reload --host $HOST --port $PORT --log-level $LOG_LEVEL
+if [ -f app/main.py ]; then
+    DEFAULT_MODULE_NAME=app.main
+elif [ -f /app/main.py ]; then
+    DEFAULT_MODULE_NAME=main
+fi
+
+MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
+VARIABLE_NAME=${VARIABLE_NAME:-app}
+export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
+
+exec nohup uvicorn --reload --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE" --access-log > logs/gamereview.log 2>&1 &
